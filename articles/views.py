b81c4e2e51996327be7211f4b1822181ddc6d8d3
Miroslav Krastev <mirokrastev@protonmail.com>
2023-09-03 21:54:28 +0300

    AllowAny for the ViewSets

--- articles/views.py
+++ articles/views.py
@@ -7,8 +7,10 @@ from articles import models
 class CategoryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
+    permission_classes = []
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.ArticleSerializer
     queryset = models.Article.objects.all()
+    permission_classes = []
